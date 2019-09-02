de7f956e8beadbc02570f29cbbc83e9c84c77864
vano <kolesen.ivan@gmail.com>
2019-09-02 21:28:34 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const pages = document.querySelector('.pages');
+const calendar = document.querySelector('.calendar');
+
+function handleClick (e) {
+  updateCalendar(e.target);
+}
+
+function updateCalendar (target) {
+  if (target && target.classList.contains('page')) {
+    target.classList.add('tear');
+    setTimeout(() => {
+      pages.removeChild(target);
+    }, 800);
+  } else {
+    return;
+  }
+  renderPage();
+}
+
+function play(){
+  calendar.removeEventListener('click', play);
+  var audio = document.getElementById("audio");
+  audio.play();
+}
+
+function renderPage () {
+  const newPage = document.createElement('div');
+  newPage.classList.add('page');
+  newPage.innerHTML = `
+    <p class="month">Сентябрь</p>
+    <p class="day">3</p>
+    <p class="day-name">Вторник</p>
+    <p class="year">2019</p>
+  `;
+  pages.appendChild(newPage);
+}
+
+renderPage();
+
+pages.addEventListener('click', handleClick);
+calendar.addEventListener('click', play);
